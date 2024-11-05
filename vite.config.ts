2dabe47b0d865342183677f1606f56aa0020ff8c
begli.amanov <begli.amanov@accenture.com>
2024-11-05 08:34:53 +0100

    Delete old workflow. Rename vue-website in all files to vue-monitor

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 			resolvers: [PrimeVueResolver()],
 		}),
 	],
-	base: '/vue-website',
+	base: '/vue-monitor',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
