d0cd9a2f65b2594c2abdbe3710677c7cb973bfd2
begli.amanov <begli.amanov@accenture.com>
2024-11-06 13:27:16 +0100

    Add margin and padding to input elements. Add export to tailwind config for font Inter Var

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,12 +3,24 @@
 <template>
 	<div class="wrapper">
 		<form class="card">
-			<h3>Welcome Back</h3>
+			<h2 class="heading-2">Welcome Back</h2>
 			<label for="email">Email </label>
 			<input type="email" name="email" id="" placeholder="Email address" />
 
 			<label for="password">Password </label>
 			<input type="password" name="password" id="" placeholder="Password" />
+
+			<div class="interactions">
+				<div class="remember">
+					<div class="checkbox">
+						<input type="checkbox" name="" id="" />
+					</div>
+
+					<label for="">Remember Me</label>
+				</div>
+
+				<a href="">Forgot your password?</a>
+			</div>
 
 			<button v-ripple class="button">
 				<span class="pi pi-user"></span> Sign In
@@ -16,3 +28,5 @@
 		</form>
 	</div>
 </template>
+
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,17 @@ export default {
 	},
 	plugins: [require('tailwindcss-primeui')],
 };
+
+module.exports = {
+	theme: {
+		fontFamily: {
+			sans: [
+				'Inter var, sans-serif',
+				{
+					fontFeatureSettings: '"cv11", "ss01"',
+					fontVariationSettings: '"opsz" 32',
+				},
+			],
+		},
+	},
+};
