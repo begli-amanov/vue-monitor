81b3833ea09f2bb88bdc77ad9bacdf583e907312
begli.amanov <begli.amanov@accenture.com>
2024-12-05 23:08:02 +0100

    Move script part of Home.vue to the top of the file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,159 @@
+<script setup lang="js">
+import { ProductService } from '@/services/ProductService';
+import { FilterMatchMode } from '@primevue/core/api';
+import Toolbar from 'primevue/toolbar';
+import { useToast } from 'primevue/usetoast';
+import { onMounted, ref } from 'vue';
+
+onMounted(() => {
+	ProductService.getProducts().then((data) => (products.value = data));
+});
+
+const toast = useToast();
+const dt = ref();
+const products = ref();
+const productDialog = ref(false);
+const deleteProductDialog = ref(false);
+const deleteProductsDialog = ref(false);
+const product = ref({});
+const selectedProducts = ref();
+const filters = ref({
+	global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+const submitted = ref(false);
+const statuses = ref([
+	{ label: 'INSTOCK', value: 'instock' },
+	{ label: 'LOWSTOCK', value: 'lowstock' },
+	{ label: 'OUTOFSTOCK', value: 'outofstock' },
+]);
+
+const formatCurrency = (value) => {
+	if (value)
+		return value.toLocaleString('en-US', {
+			style: 'currency',
+			currency: 'USD',
+		});
+	return;
+};
+const openNew = () => {
+	product.value = {};
+	submitted.value = false;
+	productDialog.value = true;
+};
+const hideDialog = () => {
+	productDialog.value = false;
+	submitted.value = false;
+};
+const saveProduct = () => {
+	submitted.value = true;
+
+	if (product?.value.name?.trim()) {
+		if (product.value.id) {
+			product.value.inventoryStatus = product.value.inventoryStatus.value
+				? product.value.inventoryStatus.value
+				: product.value.inventoryStatus;
+			products.value[findIndexById(product.value.id)] = product.value;
+			toast.add({
+				severity: 'success',
+				summary: 'Successful',
+				detail: 'Product Updated',
+				life: 3000,
+			});
+		} else {
+			product.value.id = createId();
+			product.value.code = createId();
+			product.value.image = 'product-placeholder.svg';
+			product.value.inventoryStatus = product.value.inventoryStatus
+				? product.value.inventoryStatus.value
+				: 'INSTOCK';
+			products.value.push(product.value);
+			toast.add({
+				severity: 'success',
+				summary: 'Successful',
+				detail: 'Product Created',
+				life: 3000,
+			});
+		}
+
+		productDialog.value = false;
+		product.value = {};
+	}
+};
+const editProduct = (prod) => {
+	product.value = { ...prod };
+	productDialog.value = true;
+};
+const confirmDeleteProduct = (prod) => {
+	product.value = prod;
+	deleteProductDialog.value = true;
+};
+const deleteProduct = () => {
+	products.value = products.value.filter((val) => val.id !== product.value.id);
+	deleteProductDialog.value = false;
+	product.value = {};
+	toast.add({
+		severity: 'success',
+		summary: 'Successful',
+		detail: 'Product Deleted',
+		life: 3000,
+	});
+};
+const findIndexById = (id) => {
+	let index = -1;
+	for (let i = 0; i < products.value.length; i++) {
+		if (products.value[i].id === id) {
+			index = i;
+			break;
+		}
+	}
+
+	return index;
+};
+const createId = () => {
+	let id = '';
+	var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (var i = 0; i < 5; i++) {
+		id += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+	return id;
+};
+const exportCSV = () => {
+	dt.value.exportCSV();
+};
+const confirmDeleteSelected = () => {
+	deleteProductsDialog.value = true;
+};
+const deleteSelectedProducts = () => {
+	products.value = products.value.filter(
+		(val) => !selectedProducts.value.includes(val)
+	);
+	deleteProductsDialog.value = false;
+	selectedProducts.value = null;
+	toast.add({
+		severity: 'success',
+		summary: 'Successful',
+		detail: 'Products Deleted',
+		life: 3000,
+	});
+};
+
+const getStatusLabel = (status) => {
+	switch (status) {
+		case 'INSTOCK':
+			return 'success';
+
+		case 'LOWSTOCK':
+			return 'warn';
+
+		case 'OUTOFSTOCK':
+			return 'danger';
+
+		default:
+			return null;
+	}
+};
+</script>
+
 <template>
 	<div>
 		<div>
@@ -312,159 +468,3 @@
 		</Dialog>
 	</div>
 </template>
-
-<script setup>
-import { ProductService } from '@/services/ProductService';
-import { FilterMatchMode } from '@primevue/core/api';
-import Toolbar from 'primevue/toolbar';
-import { useToast } from 'primevue/usetoast';
-import { onMounted, ref } from 'vue';
-
-onMounted(() => {
-	ProductService.getProducts().then((data) => (products.value = data));
-});
-
-const toast = useToast();
-const dt = ref();
-const products = ref();
-const productDialog = ref(false);
-const deleteProductDialog = ref(false);
-const deleteProductsDialog = ref(false);
-const product = ref({});
-const selectedProducts = ref();
-const filters = ref({
-	global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-});
-const submitted = ref(false);
-const statuses = ref([
-	{ label: 'INSTOCK', value: 'instock' },
-	{ label: 'LOWSTOCK', value: 'lowstock' },
-	{ label: 'OUTOFSTOCK', value: 'outofstock' },
-]);
-
-const formatCurrency = (value) => {
-	if (value)
-		return value.toLocaleString('en-US', {
-			style: 'currency',
-			currency: 'USD',
-		});
-	return;
-};
-const openNew = () => {
-	product.value = {};
-	submitted.value = false;
-	productDialog.value = true;
-};
-const hideDialog = () => {
-	productDialog.value = false;
-	submitted.value = false;
-};
-const saveProduct = () => {
-	submitted.value = true;
-
-	if (product?.value.name?.trim()) {
-		if (product.value.id) {
-			product.value.inventoryStatus = product.value.inventoryStatus.value
-				? product.value.inventoryStatus.value
-				: product.value.inventoryStatus;
-			products.value[findIndexById(product.value.id)] = product.value;
-			toast.add({
-				severity: 'success',
-				summary: 'Successful',
-				detail: 'Product Updated',
-				life: 3000,
-			});
-		} else {
-			product.value.id = createId();
-			product.value.code = createId();
-			product.value.image = 'product-placeholder.svg';
-			product.value.inventoryStatus = product.value.inventoryStatus
-				? product.value.inventoryStatus.value
-				: 'INSTOCK';
-			products.value.push(product.value);
-			toast.add({
-				severity: 'success',
-				summary: 'Successful',
-				detail: 'Product Created',
-				life: 3000,
-			});
-		}
-
-		productDialog.value = false;
-		product.value = {};
-	}
-};
-const editProduct = (prod) => {
-	product.value = { ...prod };
-	productDialog.value = true;
-};
-const confirmDeleteProduct = (prod) => {
-	product.value = prod;
-	deleteProductDialog.value = true;
-};
-const deleteProduct = () => {
-	products.value = products.value.filter((val) => val.id !== product.value.id);
-	deleteProductDialog.value = false;
-	product.value = {};
-	toast.add({
-		severity: 'success',
-		summary: 'Successful',
-		detail: 'Product Deleted',
-		life: 3000,
-	});
-};
-const findIndexById = (id) => {
-	let index = -1;
-	for (let i = 0; i < products.value.length; i++) {
-		if (products.value[i].id === id) {
-			index = i;
-			break;
-		}
-	}
-
-	return index;
-};
-const createId = () => {
-	let id = '';
-	var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (var i = 0; i < 5; i++) {
-		id += chars.charAt(Math.floor(Math.random() * chars.length));
-	}
-	return id;
-};
-const exportCSV = () => {
-	dt.value.exportCSV();
-};
-const confirmDeleteSelected = () => {
-	deleteProductsDialog.value = true;
-};
-const deleteSelectedProducts = () => {
-	products.value = products.value.filter(
-		(val) => !selectedProducts.value.includes(val)
-	);
-	deleteProductsDialog.value = false;
-	selectedProducts.value = null;
-	toast.add({
-		severity: 'success',
-		summary: 'Successful',
-		detail: 'Products Deleted',
-		life: 3000,
-	});
-};
-
-const getStatusLabel = (status) => {
-	switch (status) {
-		case 'INSTOCK':
-			return 'success';
-
-		case 'LOWSTOCK':
-			return 'warn';
-
-		case 'OUTOFSTOCK':
-			return 'danger';
-
-		default:
-			return null;
-	}
-};
-</script>
